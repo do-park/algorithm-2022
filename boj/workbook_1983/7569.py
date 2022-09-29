5c64f23072a120a4bcbb300f0d5da9301f5ad3f1
박도희 <dohee.pa@gmail.com>
2022-09-29 21:31:42 +0900

    22-09-29

--- boj/workbook_1983/7569.py
+++ boj/workbook_1983/7569.py
@@ -1,19 +1,33 @@
 from collections import deque
 
-
 dzs = [-1, 1, 0, 0, 0, 0]
 dys = [0, 0, -1, 1, 0, 0]
 dxs = [0, 0, 0, 0, -1, 1]
 N, M, H = map(int, input().split())
 tomatoes = [[list(map(int, input().split())) for m in range(M)] for h in range(H)]
-# 1. 전체 맵 돌면서 익은 토마토가 있는지 확인 후 Q에 넣기 (시작 지점)
-#   - 안 익은 토마토의 개수도 함께 세자
-# 2. 익은 토마토들 각각에 대해 BFS 돌리면서 count 세기
-# 3. Q가 비었을 때 종료
-# 4. 익지 않은 토마토 (0이 있는 경우) 가 있는 경우 -1 출력, 아닌 경우 count 출력
+unripe_tomatoes = 0
+Q = deque()
 
+for n in range(N):
+    for m in range(M):
+        for h in range(H):
+            if tomatoes[h][m][n] == 1:
+                Q.append([h, m, n])
+            elif tomatoes[h][m][n] == 0:
+                unripe_tomatoes += 1
 
+result = 0
+while unripe_tomatoes > 0 and len(Q) > 0:
+    temp_Q = deque()
+    while Q:
+        [z, y, x] = Q.popleft()
+        for dz, dy, dx in zip(dzs, dys, dxs):
+            nz, ny, nx = z + dz, y + dy, x + dx
+            if 0 <= nz < H and 0 <= ny < M and 0 <= nx < N and tomatoes[nz][ny][nx] == 0:
+                temp_Q.append([nz, ny, nx])
+                tomatoes[nz][ny][nx] = 1
+                unripe_tomatoes -= 1
+    Q = temp_Q
+    result += 1
 
-
-for dz, dy, dx in zip(dzs, dys, dxs):
-    print(dz, dy, dx)
+print(result if unripe_tomatoes == 0 else -1)
